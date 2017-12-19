3f8f50c321445fb87d62f702c1834ecc60f2cea5
Administrator <Administrator@LS--20170703PCE>
2017-12-19 16:15:33 +0800

    change somethings

--- Test2/Test2.cpp
+++ Test2/Test2.cpp
@@ -1,11 +1,34 @@
 // Test2.cpp : 定義主控台應用程式的進入點。
 //
 
-#include "stdafx.h"
-
-
-int main()
+/*#include "stdafx.h"
+#include<iostream>
+int fun(int, int);
+int main(void)
 {
+	using namespace std;
+	int a, b, c;
+	cout << "Please enter two numbers:";
+	cin >> a >> b;
+	c = fun(a, b);
+	cout << " c= " << c;
     return 0;
 }
 
+int fun(int a, int b)
+{
+	int c;
+	c = a + b;
+	return c;
+}*/
+#include "stdafx.h"
+#include<iostream>
+int main(void)
+{
+	using namespace std;
+	const double pi = 3.14159;
+	double s = 0;
+	s = pi*4.0*4.0;
+	cout << s << endl;
+	return 0;
+}
